0339d015567a05406c78d5551bf6ffe265487395
Stend <StendGit@gmail.com>
2020-02-08 21:08:44 +0300

    Patch PlotType demo to use SkiaControl

--- dev/ScottPlotSkia/WinformsSkiaDemosLauncher/Program.cs
+++ dev/ScottPlotSkia/WinformsSkiaDemosLauncher/Program.cs
@@ -1,9 +1,10 @@
-﻿using ScottPlotDemos;
+﻿using Harmony;
+using ScottPlotDemos;
+using ScottPlotSkia;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace WinformsSkiaDemosLauncher
 {
@@ -11,8 +12,32 @@ namespace WinformsSkiaDemosLauncher
     {
         static void Main(string[] args)
         {
+            var harmony = HarmonyInstance.Create("DemosUseSkiaControlPatch");
+
+            var original = typeof(FormPlotTypes).GetMethod("InitializeComponent", BindingFlags.NonPublic | BindingFlags.Instance);
+            var transpiler = typeof(Program).GetMethod("Transpiler");
+
+            harmony.Patch(original, null, null, new HarmonyMethod(transpiler));
+
             FormMain form = new FormMain();
             form.ShowDialog();
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Newobj)
+                {
+                    var operand = instruction.operand as ConstructorInfo;
+                    if (operand.DeclaringType.Name == "FormsPlot")
+                    {
+                        // replace {new FormsPlot()} with {new FormsPlotSkia()}
+                        instruction.operand = typeof(FormsPlotSkia).GetConstructor(new Type[] { });
+                    }
+                }
+                yield return instruction;
+            }
         }
     }
 }
