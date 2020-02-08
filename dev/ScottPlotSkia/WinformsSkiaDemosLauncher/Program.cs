742f259f0e16d17e1b5057d2370804cf960cdd2b
StendProg <53831487+StendProg@users.noreply.github.com>
2020-02-08 22:20:10 +0300

    Merge pull request #1 from StendProg/DemoProjectsWithSkia
    Demo projects with skia

---
+++ dev/ScottPlotSkia/WinformsSkiaDemosLauncher/Program.cs
@@ -0,0 +1,56 @@
+﻿using Harmony;
+using ScottPlotDemos;
+using ScottPlotSkia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace WinformsSkiaDemosLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var harmony = HarmonyInstance.Create("DemosUseSkiaControlPatch");
+
+            var demosAssembly = typeof(FormMain).GetTypeInfo().Assembly;
+            var formsInDemos = demosAssembly.GetTypes().Where(x => x.IsSubclassOf(typeof(Form)));
+
+            var transpiler = typeof(Program).GetMethod("Transpiler");
+
+            Console.WriteLine("Patching demos");
+            foreach (var fType in formsInDemos)
+            {
+                var original = fType.GetMethod("InitializeComponent", BindingFlags.NonPublic | BindingFlags.Instance);
+                harmony.Patch(original, null, null, new HarmonyMethod(transpiler));
+                Console.WriteLine($"\t{fType} - patched");
+            }
+            Console.WriteLine("Run demos");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FormMain form = new FormMain();
+            form.ShowDialog();
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Newobj)
+                {
+                    var operand = instruction.operand as ConstructorInfo;
+                    if (operand.DeclaringType.Name == "FormsPlot")
+                    {
+                        // replace {new FormsPlot()} with {new FormsPlotSkia()}
+                        instruction.operand = typeof(FormsPlotSkia).GetConstructor(new Type[] { });
+                    }
+                }
+                yield return instruction;
+            }
+        }
+    }
+}
